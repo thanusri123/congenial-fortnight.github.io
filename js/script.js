554904d04ea27627195ea1997e8ea51a75939073
Thanu Sri <60391581+thanusri123@users.noreply.github.com>
2023-09-14 16:47:43 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+// Add an event listener to the "Submit" button of the contact form
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    
+    if (form) {
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+            // You can add JavaScript code here to handle form submission,
+            // such as sending data to a server or displaying a confirmation message.
+        });
+    }
+});
 
